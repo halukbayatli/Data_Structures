68c654f8185c495721a7df2ed39e466a19330c76
halukbayatli <haluk_bayatli@hotmail.com>
2024-09-25 21:41:35 +0300

    addition and list function writed

--- LinkedList/LinkedList1.c
+++ LinkedList/LinkedList1.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct Lesson
 {
     struct Lesson *nextLesson;
-    lessonName[50];
+    char lessonName[50];
     int midtermGrade;
     int finalGrade;
 }StuLesson;
@@ -12,27 +13,105 @@ typedef struct Lesson
 typedef struct Student
 {
     struct Student *nextStu;
-    name[50];
-    surname[50];
+    char name[50];
+    char surname[50];
     int stuNumber;
-    StuLesson lessons;
+    StuLesson *lessons;
 }Stu;
 
+Stu *Students = NULL;
 
-void lessonAdd(Stu *students)
+Stu *stuAddition(Stu *students)
 {
-    while(students != NULL)
+    Stu *headStudents = students;
+    if(students == NULL)
     {
-        students = students->nextStu;
+        students = (Stu*)calloc(1,sizeof(Stu));
+        printf("Ad: ");
+        scanf("%s",students->name);
+        printf("Soyad: ");
+        scanf("%s",students->surname);
+        students->stuNumber = 240001;
+        students->lessons = NULL;
+        students->nextStu = NULL; 
+        return students;
     }
-    puts("Ad: ");
-    scanf("%s",students->name);
-    puts("Soyad: ");
-    scanf("%s",students->surname);
-    students->stuNumber = 0;
-    int lessonNumber;
-    puts("Ders Sayisi: ");
-    scanf("%d",&lessonNumber);
+    else
+    {
+        Stu *tempStudents = students;
+        while(tempStudents)
+        {
+            tempStudents = tempStudents->nextStu;
+        }
+        int number = tempStudents->stuNumber;
+        Stu *newStudents = (Stu*)calloc(1,sizeof(Stu));
+        printf("Ad: ");
+        scanf("%s",newStudents->name);
+        printf("Soyad: ");
+        scanf("%s",newStudents->surname);
+        newStudents->stuNumber = number+1;
+        newStudents->lessons = NULL;
+        newStudents->nextStu = NULL;
+        tempStudents->nextStu = newStudents;
+    }
+    return headStudents;
+}
+
+StuLesson *lessonAddition(StuLesson *lessons)
+{
+    StuLesson *headLessons = lessons;
+    if (lessons == NULL)
+    {
+        StuLesson *newlesson = (StuLesson*)calloc(1,sizeof(StuLesson));
+        printf("Ders: ");
+        scanf("%s",newlesson->lessonName);
+        printf("Vize: ");
+        scanf("%d",&newlesson->midtermGrade);
+        printf("Final: ");
+        scanf("%d",&newlesson->finalGrade);
+        newlesson->nextLesson = NULL;
+        return lessons;
+    }
+    else
+    {
+        StuLesson *tempLessons = lessons;
+        while(tempLessons)
+        {
+            tempLessons = tempLessons->nextLesson;
+        }
+        StuLesson *newlesson = (StuLesson*)calloc(1,sizeof(StuLesson));
+        printf("Ders: ");
+        scanf("%s",newlesson->lessonName);
+        printf("Vize: ");
+        scanf("%d",&newlesson->midtermGrade);
+        printf("Final: ");
+        scanf("%d",&newlesson->finalGrade);
+        newlesson->nextLesson = NULL;
+        tempLessons->nextLesson = newlesson;
+    }
+    return headLessons;
+}
+
+void listStudent(Stu *students)
+{
+    while(students)
+    {
+        printf("\n");
+        printf("    Ad: %s\n",students->name);
+        printf("    Soyad: %s\n",students->surname);
+        printf("    Numara: %d\n",students->stuNumber);
+        printf("    Dersler:\n");
+        while(students->lessons)
+        {
+            printf("        %s\n",students->lessons->lessonName);
+            printf("        %d\n",students->lessons->midtermGrade);
+            printf("        %d\n",students->lessons->finalGrade);
+            float averageGrade = (float)(students->lessons->midtermGrade)+(students->lessons->finalGrade);
+            printf("        %.2f\n",averageGrade);
+            students->lessons = students->lessons->nextLesson;
+        }
+        students = students->nextStu; 
+    }
 }
 
 void main()
@@ -40,21 +119,32 @@ void main()
     int choose;
     while(1)
     {
-        printf("        Ogrenci Ders Sistemi\n");
-        printf("    Ders Ekleme Islemleri       ---> 1\n");
-        printf("    Listeleme Islemleri         ---> 2\n");
-        printf("    Silme Islemleri             ---> 3\n");
-        printf("    Siralama Islemleri          ---> 4\n");
+        printf("        Ogrenci Bilgi Sistemi\n");
+        printf("    Ekleme Islemleri        ---> 1\n");
+        printf("    Listeleme Islemleri     ---> 2\n");
+        printf("    Silme Islemleri         ---> 3\n");
+        printf("    Siralama Islemleri      ---> 4\n");
         puts("Islem secimi: ");
         scanf("%d",&choose);
         system("cls");
         if (choose == 1)
         {
-            /* code */
+            int lessonNumber;
+            Students = stuAddition(Students);
+            printf("Ders Sayisi: ");
+            scanf("%d",&lessonNumber);
+            if(lessonNumber < 3)
+            {
+                lessonNumber = 3;
+            }
+            for(int i = 0; i < lessonNumber; i++)
+            {
+                Students->lessons = lessonAddition(Students->lessons);
+            }   
         }
         else if (choose == 2)
         {
-            /* code */
+            listStudent(Students);
         }
         else if(choose == 3)
         {
